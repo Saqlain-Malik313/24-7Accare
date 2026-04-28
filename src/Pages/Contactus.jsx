f357594a1580b6da25f8b93b59acda99186257fe
Saqlain Malik <115017610+Saqlain-Malik313@users.noreply.github.com>
2026-04-28 23:25:04 +0530

    Refactor Contactus component layout and map integration
    Updated the Contactus component to enhance the layout and improve the map display.

--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -139,15 +139,16 @@ const Contactus = () => {
           </div>
 
           {/* Right Map */}
-          <div className="w-full h-[420px]">
+                    <div className="w-full h-[300px] sm:h-[420px] rounded-xl overflow-hidden shadow-lg">
             <iframe
               title="map"
-              src="https://www.google.com/maps?q=New%20York&output=embed"
-              className="w-full h-full border-4 border-teal-200"
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3490.7953103402974!2d79.42309977551297!3d28.963795075485535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjjCsDU3JzQ5LjciTiA3OcKwMjUnMzIuNCJF!5e0!3m2!1sen!2sin!4v1777193367289!5m2!1sen!2sin"
+              className="w-full h-full border-0"
+              allowFullScreen=""
               loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
-
         </div>
       </div>
 
